0deb79acad4709212639d1873168530e0249212d
Ivan Alekseev <89075086+JustImprove@users.noreply.github.com>
2022-06-15 01:47:49 +0300

    Fix: definitions from scope to initialiser list

--- Additional/Teams/Team 2/src/h/Bartender.h
+++ Additional/Teams/Team 2/src/h/Bartender.h
@@ -6,7 +6,7 @@ class Bartender : public Worker
     public:
         Bartender() : Worker() {PouredDrinks = 0; specialisation = Specialisation::Bartender;};
         Bartender(String Name, size_t pNumber, double Salary) : 
-            Worker(Name, pNumber, Specialisation::Bartender, Salary) {PouredDrinks = 0;};
+            Worker(Name, pNumber, Specialisation::Bartender, Salary), PouredDrinks(0) {};
 
         size_t GetPouredDrinksCount() const {return PouredDrinks;};
         void PourDrink() {PouredDrinks++;};
