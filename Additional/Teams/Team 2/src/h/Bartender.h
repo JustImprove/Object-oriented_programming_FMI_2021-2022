2e1477782941d176d578d0a101c4db65e1e22dfd
Ivan Alekseev <89075086+JustImprove@users.noreply.github.com>
2022-06-15 14:00:00 +0300

    Fix: const String& (.h)

--- Additional/Teams/Team 2/src/h/Bartender.h
+++ Additional/Teams/Team 2/src/h/Bartender.h
@@ -4,8 +4,8 @@
 class Bartender : public Worker 
 {
     public:
-        Bartender() : Worker() {PouredDrinks = 0; specialisation = Specialisation::Bartender;};
-        Bartender(String Name, size_t pNumber, double Salary) : 
+        Bartender() : Worker(), PouredDrinks(0), specialisation(Specialisation::Bartender) {};
+        Bartender(const String& Name, size_t pNumber, double Salary) : 
             Worker(Name, pNumber, Specialisation::Bartender, Salary), PouredDrinks(0) {};
 
         size_t GetPouredDrinksCount() const {return PouredDrinks;};
