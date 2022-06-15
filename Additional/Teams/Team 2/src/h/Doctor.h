00f7496af1dcf104c56f917b0d2033f8ddf5a30f
Ivan Alekseev <89075086+JustImprove@users.noreply.github.com>
2022-06-15 14:04:22 +0300

    Fix: const String& (.h)

--- Additional/Teams/Team 2/src/h/Doctor.h
+++ Additional/Teams/Team 2/src/h/Doctor.h
@@ -10,7 +10,7 @@ class Doctor : public Worker
         }; 
         
         Doctor() : Worker(), CuredPatients(0), specialisation(Specialisation::Doctor), role(Role::Undefined) {}; // "Role" ще е Undefined по подразбиране
-        Doctor(String Name, size_t pNumber, double Salary, Role role) 
+        Doctor(const String& Name, size_t pNumber, double Salary, Role role) 
             : Worker(Name, pNumber, Specialisation::Doctor, Salary), role(role), CuredPatients(0) {};
 
         void ChangeSpecialisation(Role role); // One doctor will not change his specialisation without Professor (According the logic)
